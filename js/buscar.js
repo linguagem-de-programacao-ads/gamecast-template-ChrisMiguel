07fa36cde600881bd002e2cac3a6d50ee50e30c0
ChrisMiguel <christianbellei84@gmail.com>
2024-04-18 21:20:13 -0300

    Responsividade

---
+++ js/buscar.js
@@ -0,0 +1,44 @@
+async function buscar(){
+    const resposta = await fetch("https://660f44ad356b87a55c510d1e.mockapi.io/agendas");
+    const respostaDadosAgenda = await resposta.json();
+
+    console.log("Resposta: ", respostaDadosAgenda);
+
+const cards = document.getElementById("cards_games");
+
+    cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
+            return `
+            <div class="cardItem">
+                <div class="dataGame"> 
+                    <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo} 
+                </div>
+                <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+                <div class="descricao">
+                    <p> <strong>${itemAgenda.nome}</strong> </p>
+                    <p> ${itemAgenda.descricao}</p>
+                    <p><strong>Gamers: </strong></p>
+
+                    <div class="gamers">
+                    ${
+                        itemAgenda.gamers.map((gamerItem) => {
+                            return `<div class="gamerItem">@${gamerItem}</div>`
+                        }).join('')
+                    }
+
+                    </div>
+
+                  
+                </div>  
+                <div class="assistir">
+                        <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+                </div>
+            </div>
+
+            `
+            
+            
+        });
+}
+
+buscar();
+
